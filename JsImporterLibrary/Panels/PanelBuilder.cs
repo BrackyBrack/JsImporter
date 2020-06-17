947f9fdcf500c2381d0919b5670ecae55369de4e
Bracken, David <David.Bracken@tui.co.uk>
2020-06-17 08:47:36 +0100

    Corrected SeedDates for multiple panels

--- JsImporterLibrary/Panels/PanelBuilder.cs
+++ JsImporterLibrary/Panels/PanelBuilder.cs
@@ -16,13 +16,13 @@ namespace JsImporterLibrary
                 , new Panel{Name = "50B", SeedDate = new DateTime(2021, 2, 24), NumberOfDaysInPanel = 27, NumberOfDaysBetweenPanels = 29, PanelType = PanelType.MOMO }
                 , new Panel{Name = "50C", SeedDate = new DateTime(2021, 3, 31), NumberOfDaysInPanel = 27, NumberOfDaysBetweenPanels = 29, PanelType = PanelType.MOMO }
                 , new Panel{Name = "50D", SeedDate = new DateTime(2021, 3, 3), NumberOfDaysInPanel = 27, NumberOfDaysBetweenPanels = 29, PanelType = PanelType.MOMO }
-                , new Panel{Name = "50E", SeedDate = new DateTime(2021, 2, 9), NumberOfDaysInPanel = 27, NumberOfDaysBetweenPanels = 29, PanelType = PanelType.MOMO }
-                , new Panel{Name = "50F", SeedDate = new DateTime(2021, 3, 9), NumberOfDaysInPanel = 27, NumberOfDaysBetweenPanels = 29, PanelType = PanelType.MOMO }
-                , new Panel{Name = "50G", SeedDate = new DateTime(2021, 2, 16), NumberOfDaysInPanel = 27, NumberOfDaysBetweenPanels = 29, PanelType = PanelType.MOMO }
-                , new Panel{Name = "50H", SeedDate = new DateTime(2021, 3, 16), NumberOfDaysInPanel = 27, NumberOfDaysBetweenPanels = 29, PanelType = PanelType.MOMO }
+                , new Panel{Name = "50E", SeedDate = new DateTime(2020, 12, 16), NumberOfDaysInPanel = 27, NumberOfDaysBetweenPanels = 29, PanelType = PanelType.MOMO }
+                , new Panel{Name = "50F", SeedDate = new DateTime(2021, 1, 13), NumberOfDaysInPanel = 27, NumberOfDaysBetweenPanels = 29, PanelType = PanelType.MOMO }
+                , new Panel{Name = "50G", SeedDate = new DateTime(2020, 12, 23), NumberOfDaysInPanel = 27, NumberOfDaysBetweenPanels = 29, PanelType = PanelType.MOMO }
+                , new Panel{Name = "50H", SeedDate = new DateTime(2021, 1, 20), NumberOfDaysInPanel = 27, NumberOfDaysBetweenPanels = 29, PanelType = PanelType.MOMO }
                 , new Panel{Name = "501", SeedDate = new DateTime(2021, 3, 10), NumberOfDaysInPanel = 13, NumberOfDaysBetweenPanels = 15, PanelType = PanelType.TOTO }
-                , new Panel{Name = "502", SeedDate = new DateTime(2021, 3, 17), NumberOfDaysInPanel = 13, NumberOfDaysBetweenPanels = 15, PanelType = PanelType.TOTO }
-                , new Panel{Name = "503", SeedDate = new DateTime(2021, 3, 24), NumberOfDaysInPanel = 13, NumberOfDaysBetweenPanels = 15, PanelType = PanelType.TOTO }
+                , new Panel{Name = "502", SeedDate = new DateTime(2021, 3, 24), NumberOfDaysInPanel = 13, NumberOfDaysBetweenPanels = 15, PanelType = PanelType.TOTO }
+                , new Panel{Name = "503", SeedDate = new DateTime(2021, 3, 17), NumberOfDaysInPanel = 13, NumberOfDaysBetweenPanels = 15, PanelType = PanelType.TOTO }
                 , new Panel{Name = "504", SeedDate = new DateTime(2021, 3, 31), NumberOfDaysInPanel = 13, NumberOfDaysBetweenPanels = 15, PanelType = PanelType.TOTO }
                 , new Panel{Name = "75A", SeedDate = new DateTime(2021, 3, 17), NumberOfDaysInPanel = 6, NumberOfDaysBetweenPanels = 22, PanelType = PanelType.SeventyFive }
                 , new Panel{Name = "75B", SeedDate = new DateTime(2021, 3, 24), NumberOfDaysInPanel = 6, NumberOfDaysBetweenPanels = 22, PanelType = PanelType.SeventyFive }
